91e8e0a2adb19deabeded5a7f531c6a94b1299cc
AkaHitsuji <yang025@e.ntu.edu.sg>
2019-01-17 21:34:16 +0800

    Testing post request

--- cimb/js/store_data.js
+++ cimb/js/store_data.js
@@ -1,7 +1,28 @@
+function open_index() {
+    window.open("index.html","_self", false);
+}
+
 function store_in_session() {
     var accountnum = document.getElementById("userid").value;
-    console.log(accountnum);
     sessionStorage.setItem("userid", accountnum);
 
-    window.open("https://akahitsuji.github.io/cimb/index.html","_self")
+    var obj = {};
+    obj[user_id] = accountnum;
+    obj[name] = document.getElementById("username").value;
+    obj[email] = document.getElementById("email").value;
+    obj[phone] = document.getElementById("phonenumber").value;
+
+    var data = JSON.stringify( obj );
+    $.ajax({
+            url: 'https://finul-api.herokuapp.com/register',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                $('#target').html(data.msg);
+            },
+            data: data
+        });
+
+    open_index();
 }
